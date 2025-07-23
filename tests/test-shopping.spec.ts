aabba82ae7af23fbcaf727a32bef10c2ab7a722c
theerapat.demo <theerapat.demo@gmail.com>
2025-07-24 04:36:15 +0700

    refactor code

--- tests/test-shopping.spec.ts
+++ tests/test-shopping.spec.ts
@@ -1,35 +1,26 @@
 import { test, expect } from '@playwright/test'
 
-const base_url = 'https://www.on.com/en-th/shop/womens/shoes'
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.on.com/en-th/shop/womens/shoes');
+});
 
 test.describe('Test web shopping', () => {
     test('Open the web in url', async ({ page }) => {
-        await page.goto(`${base_url}`);
         const pageTitle = 'Women’s shoes';
         const titleText = page.locator('[data-test-id="pageTitle"]');
         await expect(titleText).toHaveText(pageTitle)
     });
 
     test('Click show to filter', async ({ page }) => {
-        await page.goto(`${base_url}`);
         const button = page.locator('button:has(span:text("Show filters"))');
         await expect(button).toHaveText('Show filters')
     });
 
-    test('Click Activity : Lifestyle', async ({ page }) => {
-        await page.goto(`${base_url}`);
+    test('Click Activity : Lifestyle and select color', async ({ page }) => {
         await page.getByRole('link', { name: 'Lifestyle' }).click();
+         const wrapper = page.getByTestId('main-filter-white');
+         console.log(wrapper)
     });
 
-    test('Click Color : white', async ({ page }) => {
-        await page.goto(`${base_url}`);
-        const wrapper = page.getByTestId('main-filter-white');
-        //     const text = await wrapper.textContent(); // Returns string
-        //     console.log(text)
-
-        //    // expect(wrapper).toHaveValues('white')
-
-    })
-
-})
+});
 
