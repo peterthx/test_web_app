093a242d1f4f8b8a03941c19967dfaedab1f1e23
theerapat.demo <theerapat.demo@gmail.com>
2025-07-25 20:22:06 +0700

    add file

--- tests/test-shopping.spec.ts
+++ tests/test-shopping.spec.ts
@@ -19,8 +19,25 @@ test.describe('Test web shopping', () => {
     test('Click Activity : Lifestyle and select color', async ({ page }) => {
         await page.getByRole('link', { name: 'Lifestyle' }).click();
         const wrapper = page.getByTestId('main-filter-white');
-        console.log(wrapper)
+        //console.log(wrapper)
+    });
+
+    test('Select checkbox', async ({ page }) => {
+        const ulElements = await page.locator('[data-test-id="filter-white"]').all();
+        for (let i = 0; i < ulElements.length; i++) {
+            const ul = ulElements[i];
+
+            // Get all li elements within this ul
+            const liElements = await ul.locator('li').all();
+
+            console.log(`UL ${i + 1} has ${liElements.length} items:`);
+
+            for (let j = 0; j < liElements.length; j++) {
+                const liText = await liElements[j].textContent();
+                console.log(`  Item ${j + 1}: ${liText}`);
+            }
+        }
+
     });
 
 });
-
